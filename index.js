e365f209b38c848201eeaff53deda9e6378f047a
Remo <81964236+remoremote@users.noreply.github.com>
2024-03-10 13:46:18 +0000

    save links in json

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-
 const { scrapeOverviewPage } = require("./src/scrape/scrapeOverviewPage");
 const { scrapePage } = require("./src/scrape/scrapePage");
 const { saveToJson } = require("./src/database/saveToJson");
 
-
 async function main() {
   console.log("Starting main scraping function...");
   let allDetails = [];
+  let allLinks = []; // Store all links fetched
 
   const maxPageNumber = 1; // Define maxPageNumber max is 30
 
   for (let i = 0; i <= maxPageNumber; i++) {
     const pageLinks = await scrapeOverviewPage(i);
+    allLinks = allLinks.concat(pageLinks); // Collect all links
     for (const link of pageLinks) {
       const details = await scrapePage(link);
       if (details) {
@@ -21,6 +21,9 @@ async function main() {
   }
 
   allDetails = allDetails.filter((detail) => !("Kaufpreis" in detail));
+
+  // Save links to JSON
+  await saveToJson(allLinks, "links.json"); // Saving the fetched links
 
   // Save to JSON and PostgreSQL database
   await saveToJson(allDetails, "details.json");
--- src/scrape/scrapePage.js
+++ src/scrape/scrapePage.js
@@ -1,5 +1,12 @@
-async function scrapePage(url) {
-  // Implementation here
+const { fetchHTML } = require("../utils/fetchData");
+
+async function scrapePage(link) {
+const $ = await fetchHTML(link);
+if (!$) return null;
+
+  const details = {};
+  return details;
+
 }
 
 module.exports = { scrapePage };
